1ab8df60b85d4e4fcf901413f3fa55107ca80312
BRENDON_AVISH/HP <avish.negi137@gmail.com>
2020-03-20 20:27:24 +0530

    establishing connection frontend and backend both

---
+++ CSE-B/Holi_Assignment/problem1/connect.php
@@ -0,0 +1,32 @@
+<?php
+$hostName = 'localhost';
+$userName = 'root';
+$password = '';
+$databaseName = 'student_db';
+    
+$conn = mysqli_connect($hostName, $userName, $password, $databaseName);
+if ($conn){
+}
+else{
+    echo "Connection is failed!!" + mysqli_connect_error();
+}
+if(isset($_POST['btn']))
+{
+	
+	$enroll = $_POST['enroll_no'];
+ 	$name = $_POST['name'];
+ 	$age = $_POST['age'];
+ 	$course = $_POST['course'];
+ 	$branch = $_POST['branch'];
+ 	$contact = $_POST['contact'];
+ 	$email = $_POST['email'];
+
+	$qy = "insert into `student`(enroll_no,name,age,course,branch,contact,email)values(''$enroll', $name', '$age', '$course', '$branch', '$contact', '$email'')";
+	$qt = mysqli_query($conn,$qy);
+	if($qt)
+	{
+		echo "<script>alert('Your registeration is complete')</script>";
+		echo"<script>window.open('Recordentry.php','self')</script>";
+	}
+}
+?>
